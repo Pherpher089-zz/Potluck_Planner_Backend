5d67c4c6db5fab3269031e16db4f6a7cbfff6c61
Chris <christutor089@gmail.com>
2021-10-11 01:08:33 -0700

    miguels cors fix

--- api/server.js
+++ api/server.js
@@ -16,13 +16,14 @@ var corsOptions = {
 const server = express();
 server.use(bodyParser({ extended: false }));
 server.use(helmet());
-server.use(cors(corsOptions));
+server.use(cors());
+server.options("*", cors())
 
-server.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-  });
+// server.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+//   });
 
 
 server.use("/api/auth", authRouter);
